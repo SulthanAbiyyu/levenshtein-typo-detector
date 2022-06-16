b550a01767a2ba0797c7082e8a14ae3cfdf2e5a4
Sulthan Abiyyu <zblackjack980@gmail.com>
2022-06-16 18:15:04 +0700

    refactor: added annotations

--- src/dataset.py
+++ src/dataset.py
@@ -1,5 +1,4 @@
-def dataset(path):
-
+def dataset(path: str) -> dict:
     kbbi = open(path, 'rb').read().lower().decode('utf-8').split("\r\n")
     kbbi_map = {
         **{word: "dummy_val" for word in kbbi},
