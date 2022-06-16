d3440c4c18fe09521941fd40fe81e44c274bf854
Sulthan Abiyyu <zblackjack980@gmail.com>
2022-06-16 18:02:35 +0700

    feat: dataset script

---
+++ src/dataset.py
@@ -0,0 +1,7 @@
+def dataset(path):
+
+    kbbi = open(path, 'rb').read().lower().decode('utf-8').split("\r\n")
+    kbbi_map = {
+        **{word: "dummy_val" for word in kbbi},
+    }
+    return kbbi_map
