947118b17e509b7e0aa23a1750f097bb561f275d
SulthanAbiyyu <zblackjack980@gmail.com>
2022-06-23 09:55:44 +0700

    docs: docstring for optimizer and dataset

--- src/dataset.py
+++ src/dataset.py
@@ -1,4 +1,18 @@
 def dataset(path: str) -> dict:
+    """
+    Generate dataset into dictionary, so can do random access search
+
+    Args:
+        path: path to the dataset
+    
+    Returns:
+        dataset dictionary
+
+    Raises:
+        Still None
+    """
+    # TODO: Add file note found exception
+    
     kbbi = open(path, 'rb').read().lower().decode('utf-8').split("\r\n")
     kbbi_map = {
         **{word: "dummy_val" for word in kbbi},
--- src/optimizer.py
+++ src/optimizer.py
@@ -2,6 +2,15 @@ import pandas as pd
 
 
 def word_ngram(text: str) -> list:
+    """
+    Convert word into trigram list
+
+    Args:
+        text: string that going to be converted
+    
+    Returns:
+        list of trigrammed word
+    """
     unigram = [t for t in text]
     trigram = [list(unigram[i:i+3]) for i in range(len(unigram)-2)]
     trigram_join = [''.join(t) for t in trigram]
@@ -10,10 +19,32 @@ def word_ngram(text: str) -> list:
 
 
 def word_ngram_suspect(typo_suspect: list[str]) -> list:
+    """
+    make list of word_ngram list for typo_suspect
+
+    Args:
+        typo_suspect: list of words that not found in dataset
+    
+    Returns:
+        list of word_ngram list
+    """
     return [word_ngram(gram) for gram in typo_suspect]
 
 
 def tolerated_word_ngram(search_key: list[str], typo_suspect: list[str], kbbi_map: dict, tolerated_len=2) -> list:
+    """
+    constrain possible key from dataset
+
+    Args:
+        search_key: list of word_ngram list
+        typo_suspect: list of words that not present in dataset
+        kbbi_map: dataset dictionary
+        tolerated_len: string len constrain
+
+    Returns:
+        list of possible key for all typo suspect
+    """
+    
     possible_key = []
     idx = 0
     for i in search_key:
