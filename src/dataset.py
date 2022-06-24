420a981a24abc9a7c29941bc962b2144eb9d0924
Sulthan Abiyyu <zblackjack980@gmail.com>
2022-06-24 20:39:19 +0700

    fix: gui.py

--- src/dataset.py
+++ src/dataset.py
@@ -1,11 +1,13 @@
 import requests
+
+
 def dataset(path: str) -> dict:
     """
     Generate dataset into dictionary, so can do random access search
 
     Args:
         path: path to the dataset
-    
+
     Returns:
         dataset dictionary
 
@@ -13,9 +15,9 @@ def dataset(path: str) -> dict:
         Still None
     """
     # TODO: Add file note found exception
-    
+
     kbbi = requests.get(
-        "https://raw.githubusercontent.com/SulthanAbiyyu/levenshtein-typo-detector/master/data/raw/kbbi.txt").text.split("\n")
+        path).text.split("\n")
     kbbi_map = {
         **{word: "dummy_val" for word in kbbi},
     }
